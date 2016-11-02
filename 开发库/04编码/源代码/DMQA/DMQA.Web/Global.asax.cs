91f81256344ef43b590259887fb75fc5a60048d1
Zeyu.Tian <498442999@qq.com>
2016-11-02 11:55:44 +0800

    IOC

--- 开发库/04编码/源代码/DMQA/DMQA.Web/Global.asax.cs
+++ 开发库/04编码/源代码/DMQA/DMQA.Web/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using DMQA.Infrastructure.Ioc;
+using Ninject;
 
 namespace DMQA.Web
 {
@@ -13,11 +15,18 @@ namespace DMQA.Web
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
+            ControllerBuilder.Current.SetControllerFactory(new NinjectConfig(AddBindings));
+
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);       
+        }
 
-            
+        protected void AddBindings(IKernel ninjectKernel)
+        {
+            //todo:额外的注入代码
+            //ninjectKernel.Bind<IAdapter>().To<Adapter>();
         }
     }
 }
