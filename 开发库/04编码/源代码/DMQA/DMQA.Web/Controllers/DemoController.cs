361f36c66cf642584485667a0447c76fdf67426f
Zeyu.Tian <498442999@qq.com>
2016-11-03 14:28:16 +0800

    demo

---
+++ 开发库/04编码/源代码/DMQA/DMQA.Web/Controllers/DemoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DMQA.Web.Controllers
+{
+    public class DemoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Demo
+        public ActionResult ListTree()
+        {
+            return View();
+        }
+    }
+}
