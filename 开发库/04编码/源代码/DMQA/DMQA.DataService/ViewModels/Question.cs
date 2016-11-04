4ac589133f6ce01865caaf1d8df9aa327f5e27cc
Guangxia.Wu <sylwxgg@foxmail.com>
2016-11-04 15:24:21 +0800

    ViewModel  Question

---
+++ 开发库/04编码/源代码/DMQA/DMQA.DataService/ViewModels/Question.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMQA.DataService.ViewModels
+{
+    public class Question
+    {
+        public string Code { get; set; }
+
+        public string CatalogCode { get; set; }
+
+        public string  UserCode{ get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+
+        public int? Mark { get; set; }
+
+        public DateTime? PostDatetime { get; set; }
+
+        public int? Type { get; set; }
+
+        public string _state { get; set; }
+    }
+}
